c5987a1a76179609b3a22215fd9fe2fae27f0821
Denys Tanashchuk <denys.tanashchuk@microstep-mis.com>
2024-12-19 19:27:59 +0100

    Finished implementation of interaction with localChanges, but did not test yet. Now moven on locks. Updated .md

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Simple-database-server
+# Simple database server
+**Simple data server program, which can store and retrieve data from a file**
+
+---
+# Visual representation of the data structure in the file
+
+So basically data are stored in Map<String List<String>> structure. The key is the name of the table and the value is the list of values in the table.
+
+
+| Table 1 | Table 2 | Table 3 | ...     |
+|---------| ------- | ------- | ------- |
+| Value 1 | Value 1 | Value 1 | ...     |
+| Value 2 | Value 2 | Value 2 | ...     |
+| Value 3 | Value 3 | Value 3 | ...     |
+| ...     | ...     | ...     | ...     |
+---
+
+
+# Plans on future improvements
+
+
+- More advanced data storage
+- GUI for the server
+- Graphical representation of the data
+- Graphical representation of statistics (memory, load, connections, etc.)
+- Probably something with the web (REST API, etc.)
+- Need to think of something special to add to the project :)
--- src/database/TransactionHandler.java
+++ src/database/TransactionHandler.java
@@ -5,6 +5,7 @@ import java.net.Socket;
 import java.util.*;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
+import java.util.stream.*;
 
 public class TransactionHandler implements Runnable{
 
@@ -12,7 +13,7 @@ public class TransactionHandler implements Runnable{
     private final Socket socket;
     private PrintWriter writer;
     BufferedReader reader;
-    private Map<String, List<String>> localChanges;
+    private final Map<String, List<String>> localChanges;
 
     private final Map<String, List<String>> data;
     private final ArrayList<String> transaction = new ArrayList<>();
@@ -21,10 +22,12 @@ public class TransactionHandler implements Runnable{
     private final Map<String, Consumer<List<String>>> commandHandlers = new HashMap<>();
     private final Map<String, Runnable> specialCommands = new HashMap<>();
 
+    private final ArrayList<String> synchronizedTabels = new ArrayList<>();
+
     public TransactionHandler(Socket socket, Map<String, List<String>> data) throws IOException {
         this.socket = socket;
         this.data = data;
-        localChanges = new HashMap<>(data);
+        localChanges = new HashMap<>();
 
         commandHandlers.put("CREATE", this::handleCreateCommand);
         commandHandlers.put("SELECT", this::handleSelectCommand);
@@ -36,6 +39,18 @@ public class TransactionHandler implements Runnable{
     }
     private void commitHandler(){
         transaction.forEach(this::handleCommand);
+
+        //Zapisanie do globalnej databazy
+        synchronized (data) {
+
+            synchronizedTabels.forEach(table -> {
+                data.put(table, localChanges.get(table));
+            });
+
+            localChanges.keySet().stream().filter(table -> !synchronizedTabels.contains(table))
+                    .forEach(table -> data.get(table).addAll(localChanges.get(table)));
+        }
+
         System.out.println("Commited.");
         writer.print("Commited. \n");
         writer.flush();
@@ -74,32 +89,54 @@ public class TransactionHandler implements Runnable{
     }
     private void handleCreateCommand(List<String> parts){
         String tableName = parts.get(2);
-        data.put(tableName, new ArrayList<>());
+        localChanges.put(tableName, new ArrayList<>());
     }
     private void handleSelectCommand(List<String> parts){
         String tableName = parts.get(2);
+
         String filterCriteria = parts.stream()
                 .skip(4)
                 .findFirst()
                 .orElse("");
+        //Viem, ze by sa toto dalo aj tak, ale nie som si isty, ze to sa nepovažuje za "if"
+        // String filterCriteria = parts.size() > 4 ? parts.get(4) : "";
+
+        mergeTables(tableName);
+
+        System.out.println("Merged table: " + tableName + " " + localChanges.get(tableName));
         writer.print("Words from table " + tableName + " that starts with " + filterCriteria + ": ");
             writer.println(
-                    data.get(tableName).stream()
+                    localChanges.get(tableName).stream()
                             .filter(word -> word.startsWith(filterCriteria))
                             .collect(Collectors.joining(", ")));
             writer.flush();
     }
     private void handleInsertCommand(List<String> parts){
         String tableName = parts.get(2);
-        data.get(tableName).addAll(List.of(parts.get(4).split(",")));
+        mergeTables(tableName);
+        localChanges.get(tableName).addAll(List.of(parts.get(4).split(",")));
     }
     private void handleDeleteCommand(List<String> parts){
         String tableName = parts.get(2);
+        mergeTables(tableName);
         try{
-            data.get(tableName).removeIf(word -> word.startsWith(parts.get(4)));
+            localChanges.get(tableName).removeIf(word -> word.startsWith(parts.get(4)));
         }catch(IndexOutOfBoundsException e){
-            data.get(tableName).clear();
+            localChanges.get(tableName).clear();
         }
+    }
+
+    //Vybral som si takyto sposob synchronizacie, lebo podla mna je kopirovat celu databazu do localChanges
+    //moze byt casovo narocne pri velkych databazach, preto si beirem len potrebne tabulky.
+    private void  mergeTables(String tableName){
+        List<String> globalData = data.getOrDefault(tableName, new ArrayList<>());
+        List<String> localData = localChanges.getOrDefault(tableName, new ArrayList<>());
+
+        // Merge without duplicates
+        localChanges.put(tableName, Stream.concat(globalData.stream(), localData.stream())
+                                        .distinct()
+                                        .collect(Collectors.toList()));
+        synchronizedTabels.add(tableName);
     }
 
     @Override
